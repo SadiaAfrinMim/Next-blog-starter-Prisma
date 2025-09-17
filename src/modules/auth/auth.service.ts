44c5b47a525bac6f0172922da901120895193717
Sadia Afrin Mim <mimsadia367@gmail.com>
2025-09-17 16:28:08 +0600

    login and registration complete

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { prisma } from "../../config/db"
+
+
+const loginWithEmailAndPassword = async ({email,password}:{email:string,password:string})=>{
+    console.log({email,password})
+
+    const user = await prisma.user.findUnique({
+        where:{
+            email
+        }
+    });
+    if(!user){
+        throw new Error("user not found")
+    }
+    if(password === user.password){
+        return user
+    }
+    else{
+         throw new Error("password is incorrect")
+
+    }
+
+}
+
+export const AuthService ={
+    loginWithEmailAndPassword
+}
